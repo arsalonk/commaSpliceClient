49b63e2af43639b5b316cbc7a0b846c0fe126292
Sayed Khan <arsalon47@gmail.com>
2018-07-13 15:36:32 -0400

    question form given its own component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,61 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm, focus,reset } from 'redux-form';
-import Input from './input';
-import { required, nonEmpty } from '../validators';
 import requiresLogin from './requires-login';
-import { submitAnswer } from '../actions/auth';
-import { displayAnswer } from '../actions/feedback';
 import Feedback from './feedback';
 
 import './styles/dashboard.css';
+import Question from './question';
 
 
 export class Dashboard extends React.Component {
 
-    onSubmit(values) {
-        if (values.answer === this.props.question.answer) {
-            console.log('correct')
-            this.props.dispatch(displayAnswer(true));
-        } 
-        if (values.answer !== this.props.question.answer) {
-            console.log('incorrect')
-            this.props.dispatch(displayAnswer(false));
-        }
-        this.props.dispatch(submitAnswer(values.answer, this.props.id));
-        this.props.dispatch(reset('answer'));
-    }
-
     render() { 
         if(!this.props.loading && this.props.currentAnswer === null) {return (
-            <div className='text-container main-display'>
-                <form
-                    id='input-form'
-                    onSubmit={this.props.handleSubmit(values =>
-                        this.onSubmit(values)
-                    )}>
-                    <h2 className='farsi-display'>{this.props.question.question}</h2>
-                    <p>({this.props.question.pronounce})</p>
-                    <p>Write the English translation below</p>
-                    <Field
-                        component={Input}
-                        type="text"
-                        name="answer"
-                        id="answer"
-                        validate={[required, nonEmpty]}
-                        autoComplete="off"
-                        label='translation:'
-                        enableReinitialize="true"
-                    />
-                    <button className='form-button'>check</button>
-                </form>            
-            </div>
+            <Question />
         )}
         return(
-           <Feedback 
-                answer={this.props.question.answer}
-                correct={this.props.currentAnswer}
-            />         
+           <Feedback />         
         )
 
 
@@ -71,7 +30,4 @@ const mapStateToProps = state => {
     };
 };
 
-export default requiresLogin()(reduxForm({
-    form: 'answer',
-    onSubmitFail: (errors, dispatch) => dispatch(focus('answer'))
-})(connect(mapStateToProps)(Dashboard)));
+export default requiresLogin()(connect(mapStateToProps)(Dashboard));
